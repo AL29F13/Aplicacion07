6be3727ac703364620e754acede26ae26aed5140
AL29F13 <manuntd.rg11@gmail.com>
2015-06-02 13:45:54 -0500

    CUATRO
    aud

--- js/acciones.js
+++ js/acciones.js
@@ -1,6 +1,11 @@
 // JavaScript Document
 $(document).ready(function(e) {
 //document.addEventListener("deviceready",function(){
+	audio=window.plugins.LowLatencyAudio;
+	audio.preloadFX('B1', 'audio/C.mp3', function(){}, function(msg){alert("Error "+msg);});
+	audio.preloadFX('B2', 'audio/D.mp3', function(){}, function(msg){alert("Error "+msg);});
+	audio.preloadFX('B3', 'audio/E.mp3', function(){}, function(msg){alert("Error "+msg);});
+	audio.preloadFX('B4', 'audio/F.mp3', function(){}, function(msg){alert("Error "+msg);});
 	$('#btnjugar').on('click',function(){
 		var pantalla = $.mobile.getScreenHeight();
 		var encabezado = $('.ui-header').outerHeight(); 
