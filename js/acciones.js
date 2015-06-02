1d83eb2378f778c7e4b38f725f6015b867347786
AL29F13 <manuntd.rg11@gmail.com>
2015-06-02 14:44:39 -0500

    JGFX

--- js/acciones.js
+++ js/acciones.js
@@ -1,25 +1,43 @@
 // JavaScript Document
 $(document).ready(function(e) {
-//document.addEventListener("deviceready",function(){
+document.addEventListener("deviceready",function(){
 	audio=window.plugins.LowLatencyAudio;
-	audio.preloadFX('B1', 'audio/C.mp3', function(){}, function(msg){alert("Error "+msg);});
-	audio.preloadFX('B2', 'audio/D.mp3', function(){}, function(msg){alert("Error "+msg);});
-	audio.preloadFX('B3', 'audio/E.mp3', function(){}, function(msg){alert("Error "+msg);});
-	audio.preloadFX('B4', 'audio/F.mp3', function(){}, function(msg){alert("Error "+msg);});
-	$('#btnjugar').on('click',function(){
-		var pantalla = $.mobile.getScreenHeight();
-		var encabezado = $('.ui-header').outerHeight(); 
-		var pie = $('.ui-footer').outerHeight();
-		var contenido = $('.ui-content').outerHeight();
-		var alto = ((pantalla - encabezado - pie)/2);
-		$('.cuadro').height( alto);
-		$('.cuadro').on('mousedown', function (){
-			$(this).addClass('pulsado');
-			
-		});
+	audio.preloadFX('B1','audio/C.mp3',function(){},
+	function(msg){alert("Error "+msg);});
+	audio.preloadFX('B2','audio/D.mp3',function(){},
+	function(msg){alert("Error "+msg);});
+	audio.preloadFX('B3','audio/E.mp3',function(){},
+	function(msg){alert("Error "+msg);});
+	audio.preloadFX('B4','audio/F.mp3',function(){},
+	function(msg){alert("Error "+msg);});
+	
+	$('#btnjugar').on('tap',function(){
+		var pantalla=$.mobile.getScreenHeight();
+		var encabezado=$('.ui-header').outerHeight();
+		var pie=$('.ui-footer').outerHeight();
+		var contenido=$('.ui-content').outerHeight();
+		var alto=(pantalla - encabezado - pie)/2
+		//alert('Pantalla  '+ pantalla)
+		//alert('Encabezado  '+ encabezado)
+		//alert('Pie  '+ pie)
+		//alert('Contenido  '+ contenido)
+		//alert('Alto ' + alto)
+		$('.cuadro').height(alto);
 		
-});//btnjugar.click
-//}); 
+});//btnjugar on click
+$('.cuadro').on('vmousedown',function(){
+	$('#pantalla').append(quien($(this).attr('id')));
+			$(this).addClass('pulsado')
+			
 });
-
-
+$('.cuadro').on('vmouseup',function(){
+			$(this).removeClass('pulsado')
+			
+});
+function quien (q)
+{
+	audio.play(q);
+	return q.substring(1);
+}
+}); 
+});
